906d1957543de8fc184966c898beea3ebb7d2871
Nathan Bertram <bentspacetime@gmail.com>
2016-03-04 19:01:27 -0800

    created matchArgCombination() function

--- bankacctp2/main.cpp
+++ bankacctp2/main.cpp
@@ -8,11 +8,11 @@ COMPILER:          Xcode, GCC
 
 NOTES:             Put other information here ...
 
-MODIFICATION HISTORY: created sortArgCollection() function
+MODIFICATION HISTORY: created matchArgCombination() function
 
 Author                  Date               Version
 ---------------         ----------         --------------
-Nathan Bertram          2016-03-04         Version 4.0
+Nathan Bertram          2016-03-04         Version 5.0
 
 ----------------------------------------------------------------------------- */
 
@@ -27,14 +27,28 @@ void helpScreen();
 void checkForNoArgs(int);
 void checkForSlashes(int, char * []);
 void collectFirstCharArgs(int, char * [], char []);
-void sortArgCollection(char argCollection [], int argc);
+void sortArgCollection(char [], int);
+void matchArgCombination(char [], int);
 void displayErrorMessage();
 
 // Global Constants
 const char SLASH = '/';
 
+// Valid command line parameter combinations
+const char HELP [] = "?";
+const char DISPLAY_INFO [] = "DINP";
+const char CHANGE_FIRST_NAME [] = "DFNP";
+const char CHANGE_LAST_NAME [] = "DLNP";
+const char CHANGE_MIDDLE_INTIAL [] = "DMNP";
+const char CHANGE_SSN [] = "DNPS";
+const char CHANGE_AREA_CODE [] = "ADNP";
+const char CHANGE_PHONE_NUMBER [] = "DHNP";
+const char TRANSFER [] = "DNNPPT";
+const char CHANGE_PASSWORD [] = "DNPW";
+const char PRODUCE_REPORT [] = "DR";
+
 /* -----------------------------------------------------------------------------
-FUNCTION NAME:     name-of-function()
+FUNCTION NAME:     main()
 PURPOSE:           Purpose of function ...
 RETURNS:           What the function returns ... or ...
 RETURNS:           Nothing (void function)
@@ -42,11 +56,12 @@ NOTES:             Put important usage notes here ...
 ----------------------------------------------------------------------------- */
 int main(int argc, char * argv[]) {
     char argCollection [argc];
-    std::cout << "argc = " << argc << std::endl;
+    //std::cout << "argc = " << argc << std::endl;
     checkForNoArgs(argc);
     checkForSlashes(argc, argv);
     collectFirstCharArgs(argc, argv, argCollection);
     sortArgCollection(argCollection, argc);
+    matchArgCombination(argCollection, argc);
     return 0;
 }
 
@@ -73,6 +88,7 @@ NOTES:             Put important usage notes here ...
 void helpScreen()
 {
     std::cout << "I'm a help screen";
+    exit(0);
 }
 
 /* -----------------------------------------------------------------------------
@@ -113,10 +129,10 @@ void collectFirstCharArgs(int argc, char * argv[], char argCollection [])
         argCollection[i - 1] = argument[1]; // Add letter to arg collectoin
     }
     argCollection[argc - 1] = '\0';  // Set the last element of the array to the null byte
-    for (int i = 0; i < argc; i++)
-    {
-        std::cout << "argCollection[i]" <<argCollection[i] << std::endl;
-    }
+//    for (int i = 0; i < argc; i++)
+//    {
+//        std::cout << "argCollection[i]" <<argCollection[i] << std::endl;
+//    }
 }
 
 /* -----------------------------------------------------------------------------
@@ -130,19 +146,53 @@ void sortArgCollection(char argCollection [], int argc)
 {
     // sorts from first parameter to second parameter
     std::sort(argCollection, argCollection + (argc - 1));
-    for (int i = 0; i < argc; i++)
-    {
-        std::cout << "argCollection[i]" <<argCollection[i] << std::endl;
-    }
+//    for (int i = 0; i < argc; i++)
+//    {
+//        std::cout << "argCollection[i]" <<argCollection[i] << std::endl;
+//    }
     
 }
 /* -----------------------------------------------------------------------------
-FUNCTION NAME:     validateArgCombination()
+FUNCTION NAME:     matchArgCombination()
 PURPOSE:           Purpose of function ...
 RETURNS:           What the function returns ... or ...
 RETURNS:           Nothing (void function)
 NOTES:             Put important usage notes here ...
 ----------------------------------------------------------------------------- */
+void matchArgCombination(char argCollection [], int argc)
+{
+    if (strcmp(argCollection, HELP) == 0)
+        helpScreen();
+    else if (strcmp(argCollection, DISPLAY_INFO) == 0)
+        std::cout << "displayInfo()";
+    else if (strcmp(argCollection, CHANGE_FIRST_NAME) == 0)
+        std::cout << "changeFirstName()";
+    else if (strcmp(argCollection, CHANGE_LAST_NAME) == 0)
+        std::cout << "changeLastName()";
+    else if (strcmp(argCollection, CHANGE_MIDDLE_INTIAL) == 0)
+        std::cout << "changeMiddleIntial()";
+    else if (strcmp(argCollection, CHANGE_SSN) == 0)
+        std::cout << "changeSSN()";
+    else if (strcmp(argCollection, CHANGE_AREA_CODE) == 0)
+        std::cout << "changeAreaCode()";
+    else if (strcmp(argCollection, CHANGE_PHONE_NUMBER) == 0)
+        std::cout << "changePhoneNumber()";
+    else if (strcmp(argCollection, TRANSFER) == 0)
+        std::cout << "transfer()";
+    else if (strcmp(argCollection, CHANGE_PASSWORD) == 0)
+        std::cout << "changePassword()";
+    else if (strcmp(argCollection, PRODUCE_REPORT) == 0)
+        std::cout << "produceReport()";
+    else
+        displayErrorMessage();
+}
+/* -----------------------------------------------------------------------------
+ FUNCTION NAME:     validateArgCombination()
+ PURPOSE:           Purpose of function ...
+ RETURNS:           What the function returns ... or ...
+ RETURNS:           Nothing (void function)
+ NOTES:             Put important usage notes here ...
+ ----------------------------------------------------------------------------- */
 
 /* -----------------------------------------------------------------------------
 FUNCTION NAME:     displayErrorMessage()
@@ -153,6 +203,6 @@ NOTES:             Put important usage notes here ...
 ----------------------------------------------------------------------------- */
 void displayErrorMessage()
 {
-    std::cout << "\n\nSorry that was not a valid parameter, please try again\n\n";
+    std::cout << "\nSorry those were not a valid parameters, please try again\n\n";
     exit(42);
 }
